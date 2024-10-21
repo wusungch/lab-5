84a7868e7021fb72e1c31d30aaaed1168071ddb7
kayzoo8 <kk.zhou@mail.utoronto.ca>
2024-10-21 14:11:02 -0400

    completed todo tasks in LogoutController.java

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -25,7 +25,6 @@ public class LogoutController {
         // 1. instantiate the `LogoutInputData`, which should contain the username.
         // 2. tell the Interactor to execute.
         final LogoutInputData logoutInputData = new LogoutInputData(username);
-
         logoutUseCaseInteractor.execute(logoutInputData);
     }
 }
