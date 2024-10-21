51b009cb9d9f95eb5902b47de5620f07fd0f4faf
kayzoo8 <kk.zhou@mail.utoronto.ca>
2024-10-21 13:59:07 -0400

    completed tasks for LogoutController.java

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,6 +1,8 @@
 package interface_adapter.logout;
 
+// import use_case.login.LoginInputData;
 import use_case.logout.LogoutInputBoundary;
+import use_case.logout.LogoutInputData;
 
 /**
  * The controller for the Logout Use Case.
@@ -11,6 +13,7 @@ public class LogoutController {
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
         // TODO: Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
@@ -21,5 +24,8 @@ public class LogoutController {
         // TODO: run the use case interactor for the logout use case
         // 1. instantiate the `LogoutInputData`, which should contain the username.
         // 2. tell the Interactor to execute.
+        final LogoutInputData logoutInputData = new LogoutInputData(username);
+
+        logoutUseCaseInteractor.execute(logoutInputData);
     }
 }
